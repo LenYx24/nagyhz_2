1d7a0b4532f56e7b4829e9d8cd428c1869db8eb3
LenYx24 <fokilenard@gmail.com>
2024-04-14 23:45:47 +0200

    change folder structure, and building into the build folder

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,17 @@
-#include <SDL2/SDL.h>
-#include <iostream>
+#include <SFML/Graphics.hpp>
 
 int main() {
-  std::cout << "hello world" << std::endl;
-  SDL_Init(0);
-  return 0;
+  auto window = sf::RenderWindow{{1920u, 1080u}, "CMake SFML Project"};
+  window.setFramerateLimit(144);
+
+  while (window.isOpen()) {
+    for (auto event = sf::Event{}; window.pollEvent(event);) {
+      if (event.type == sf::Event::Closed) {
+        window.close();
+      }
+    }
+
+    window.clear();
+    window.display();
+  }
 }
