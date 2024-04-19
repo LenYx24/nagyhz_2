e103fcb1408636a085e8effd2ecf05749ee061c3
LenYx24 <fokilenard@gmail.com>
2024-04-19 16:22:35 +0200

    changing the linux and windows run scripts

--- linux_run.sh
+++ linux_run.sh
@@ -1,2 +1,7 @@
-cmake -S . -B ../linuxbuild -DCMAKE_BUILD_TYPE=Release
+var="Debug"
+if [ "$#" -ne 0 ]; then
+  var=$1
+  echo $1
+fi
+cmake -S . -B ../linuxbuild -DCMAKE_BUILD_TYPE=$var
 cmake --build ../linuxbuild
