cb144d263c16e3701e9519e41d2e4b551012ff6a
LenYx24 <fokilenard@gmail.com>
2024-04-19 14:11:47 +0200

    added .clang-format file and changed tabwidth to 4

--- src/engine.cpp
+++ src/engine.cpp
@@ -2,25 +2,25 @@
 
 Engine::Engine(sf::VideoMode winsize)
     : window(sf::RenderWindow(winsize, "lolmacrogame")), size(winsize) {
-  is_running = true;
+    is_running = true;
 }
 
 void Engine::ChangeState(State &state) {
-  if (!states.empty()) {
-    delete states.back();
-    states.pop_back();
-  }
-  states.push_back(&state);
+    if (!states.empty()) {
+        delete states.back();
+        states.pop_back();
+    }
+    states.push_back(&state);
 }
 void Engine::PushState(State &state) { states.push_back(&state); }
 void Engine::PopState() {
-  if (!states.empty())
-    states.pop_back();
+    if (!states.empty())
+        states.pop_back();
 }
 void Engine::Update() { states.back()->Update(*this); }
 void Engine::Draw() { states.back()->Draw(*this); }
 void Engine::HandleEvents() { states.back()->HandleEvents(*this); }
 void Engine::exit() {
-  window.close();
-  is_running = false;
+    window.close();
+    is_running = false;
 }
