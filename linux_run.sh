ad39edbde9d0b6f2d24604b60796d947147ec4b3
LenYx24 <fokilenard@gmail.com>
2024-04-19 16:31:50 +0200

    added subfolder for linux and windows build, changed gitignore accordingly

--- linux_run.sh
+++ linux_run.sh
@@ -3,5 +3,5 @@ if [ "$#" -ne 0 ]; then
   var=$1
   echo $1
 fi
-cmake -S . -B ../linuxbuild -DCMAKE_BUILD_TYPE=$var
-cmake --build ../linuxbuild
+cmake -S . -B ./linuxbuild -DCMAKE_BUILD_TYPE=$var
+cmake --build ./linuxbuild
