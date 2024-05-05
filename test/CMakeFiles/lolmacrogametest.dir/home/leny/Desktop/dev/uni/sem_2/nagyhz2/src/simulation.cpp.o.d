a7adada467f8535400782772a2608c505a02fea4
leny <fokilenard@gmail.com>
2024-05-05 17:36:21 +0200

    changed cmakelists file to -werror on linux

--- test/CMakeFiles/lolmacrogametest.dir/home/leny/Desktop/dev/uni/sem_2/nagyhz2/src/simulation.cpp.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/lolmacrogametest.dir/home/leny/Desktop/dev/uni/sem_2/nagyhz2/src/simulation.cpp.o: \
- /home/leny/Desktop/dev/uni/sem_2/nagyhz2/src/simulation.cpp \
- /usr/include/stdc-predef.h
